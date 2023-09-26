c02718f35df26ac0640273a0a8baa2032fcd9a47
paaaaattt <145470405+paaaaattt@users.noreply.github.com>
2023-09-26 08:40:39 -0600

    Add files via upload

---
+++ ope/src/main/java/com/mycompany/ope/Ope.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ope;
+import.javax.swing.JOptionPane;
+
+/**
+ *
+ * @author User
+ */
+public class Ope {
+
+    public static void main(String[] args) {
+        double ope1, pe2, r;
+        String ops1, ops2;
+        JOptionPane.showMessageDialog(null, "suma");
+        ops1=JOptionPane.showInputDialog("Ingresa el primer numero");
+    }
+}
